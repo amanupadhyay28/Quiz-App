0c858d0ad013b33bd9aa1193fba05722e81391aa
Patrisson <aman2833617@gmail.com>
2024-07-09 01:30:37 +0530

    final fixes

---
+++ src/data/questions.js
@@ -0,0 +1,22 @@
+export const questions = [
+    {
+      text: 'What is the capital of France?',
+      options: [
+        { text: 'Berlin', isCorrect: false },
+        { text: 'Madrid', isCorrect: false },
+        { text: 'Paris', isCorrect: true },
+        { text: 'Lisbon', isCorrect: false },
+      ],
+    },
+    {
+      text: 'Who is the CEO of Tesla?',
+      options: [
+        { text: 'Bill Gates', isCorrect: false },
+        { text: 'Elon Musk', isCorrect: true },
+        { text: 'Jeff Bezos', isCorrect: false },
+        { text: 'Mark Zuckerberg', isCorrect: false },
+      ],
+    },
+    
+  ];
+  
